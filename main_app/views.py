580be33176b513fb6720649d591c201b24a7ed67
DiAnne Lozano <diannelozano@DiAnnes-MacBook-Pro-2.local>
2021-10-18 21:59:12 -0700

    models.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,11 @@
 from django.shortcuts import render
+from .models import Fish
 
 # Create your views here.
 # Add the following import
 from django.http import HttpResponse
 
-class Fish: #Note that parens are optional if not inheriting from another class
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
 
-fishes = [
-    Fish('McFlurry', 'beta', 'red long tail', 2),
-    Fish('Sachi', 'angel fish', 'fierce angel', 1),
-    Fish('Puff Daddy', 'puffer fish', 'bad boy fo life', 2)
-]
 # Define the home view
 def home(request):
     return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
@@ -25,5 +15,6 @@ def about(request):
 
 #Add a new view
 def fishes_index(request):
+    fishes = Fish.objects.all()
     return render(request, 'fishes/index.html', {'fishes': fishes})
 
