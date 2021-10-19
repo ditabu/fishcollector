3b2269c25cb5e0cf0490ceb84d91b5f83a416794
DiAnne Lozano <diannelozano@DiAnnes-MacBook-Pro-2.local>
2021-10-18 20:08:21 -0700

    index.html template

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    #route for fish index
+    path('fishes/', views.fishes_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,8 +3,27 @@ from django.shortcuts import render
 # Create your views here.
 # Add the following import
 from django.http import HttpResponse
+
+class Fish: #Note that parens are optional if not inheriting from another class
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+fishes = [
+    Fish('McFlurry', 'beta', 'red long tail', 2),
+    Fish('Sachi', 'angel fish', 'fierce angel', 1),
+    Fish('Puff Daddy', 'puffer fish', 'bad boy fo life', 2)
+]
 # Define the home view
 def home(request):
     return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
+def about(request):
+    return render(request, 'about.html')
+
+#Add a new view
+def fishes_index(request):
+    return render(request, 'fishes/index.html', {'fishes': fishes})
 
